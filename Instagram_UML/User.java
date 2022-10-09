dd1e664d6ca159088d12481ce7708c8d189a8746
LeeJunSeo <77109803+ljs7143@users.noreply.github.com>
2022-10-09 17:37:08 +0900

    Create User.java

---
+++ Instagram_UML/User.java
@@ -0,0 +1,44 @@
+public class User{
+    private String UID;
+    public String User_ID;
+    private String password;
+    public String Email_Address;
+
+    //what users can do? 
+    //need to specify arguments later
+
+
+    //maybe it should be abstract class 
+    //then implements on  Posting table
+
+
+
+ 
+    private boolean Login(){
+        return true;
+    }
+
+    private boolean SignUp(){
+        return ture;
+        
+    }
+
+    // public WritePosts(){
+    //     //write codes later what should I implement.
+    //     //maybe contents, photos...
+    //     //using views?  
+    //     return null;
+    // }
+
+    // public WriteComments(){
+
+
+
+    // }
+
+
+
+
+
+
+}
