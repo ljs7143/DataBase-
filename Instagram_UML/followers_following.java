3bb6821d03e855ba2bd979bf516d355ff1a96c09
LeeJunSeo <77109803+ljs7143@users.noreply.github.com>
2022-10-10 22:24:12 +0900

    Update followers_following.java

--- Instagram_UML/followers_following.java
+++ Instagram_UML/followers_following.java
@@ -1,8 +1,27 @@
-import java.util.List;
+import java.util.ArrayList;
 
+//완료
 
 public class followers_following{
-    public String User_ID;
-    List<User> followers;
-    List<User> follwing;
+    public String user_ID;
+    public String following_user_id;
+    public String followers_user_id;
+
+    ArrayList<User> followers = new ArrayList<User>();   
+    ArrayList<User> following = new ArrayList<User>();
+    
+    public void follow(String user_ID, String following_user_id){
+        following.add(following_user_id);
+        
+    }
+
+    public void follower(String user_ID, String followers_user_id){
+        followers.add(followers_user_id);
+        
+    }       
+
+    
+        
+        
+    
 }
