5f427e5c77c673326952388d840cf97eb5cae307
LeeJunSeo <77109803+ljs7143@users.noreply.github.com>
2022-10-10 22:25:15 +0900

    Create Like.java

---
+++ Instagram_UML/Like.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+
+public class Like{
+    public String like_uri;
+    public String someone_Like;
+    // public String post_url;
+    boolean like = ture;
+
+
+
+//post url, 좋아하는 사람, Like를 눌렀는지 여부를 매개변수로 받고 Like 개수저장하는 postlike arraylist에 추가 
+    public void ifsomeonelikeposts(String url, String someone_Like, boolean like){
+        postlike.add("Like");
+    }
+
+    
+}
