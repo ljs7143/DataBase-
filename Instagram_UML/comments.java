f1edb704fb1826652830b7ec55132532082ad9a9
LeeJunSeo <77109803+ljs7143@users.noreply.github.com>
2022-10-10 22:24:59 +0900

    Create comments.java

---
+++ Instagram_UML/comments.java
@@ -0,0 +1,18 @@
+public comments{
+    public String comments_uri;
+    public String writer_id;
+    // public Stirng post_url;
+    //posting에서 final형으로 선언해서 주석처리
+    //contents 다른 클래스의 자료형으로 선언하는 것도 추후 고려사항
+    public String contents_wrote_to_comments_posts;
+    public String contents_wrote_to_comments_stories;
+
+
+    public void write_comments_to_posts(String url, String comments_uri, String writer_id ){
+        return contents_wrote_to_comments_posts;
+    }
+
+    void write_comments_to_posts(url, Stirng comments_uri, String writer_id){
+        return contents_wrote_to_comments_stories;l
+    }
+}
