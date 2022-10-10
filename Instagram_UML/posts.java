222d7ba0e16aec523c0ba715a3c8ffe2feb4c855
LeeJunSeo <77109803+ljs7143@users.noreply.github.com>
2022-10-10 22:26:32 +0900

    Create posts.java

---
+++ Instagram_UML/posts.java
@@ -0,0 +1,34 @@
+public class posts extends Posting{
+    public posts(String url, String user_id, String contents, String photos_url, String videos_url) {
+        super();
+        //TODO Auto-generated constructor stub
+    }
+    
+    
+    public String comments; 
+    public boolean Like;
+    public int howmanyLikes=0;
+
+
+
+
+    public void UserViews(){
+        // System.out.println(//User가보는규격대로 출력);
+
+    }
+
+    //차라리 interaction을 담당하는 클래스를 만들까?  
+
+    // public void WritingComments(String comments){
+    //     System.out.println(user_id + "님이 작성한 댓글입니다");
+    //     System.out.println(comments);
+
+    // }
+
+    // public void LikeSomeonePosts(boolean Like){
+
+
+    // }
+
+
+}
