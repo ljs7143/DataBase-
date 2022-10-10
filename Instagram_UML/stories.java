ee590ade042f5b1bac7257bb7787fca199a14309
LeeJunSeo <77109803+ljs7143@users.noreply.github.com>
2022-10-10 22:24:40 +0900

    Update stories.java

--- Instagram_UML/stories.java
+++ Instagram_UML/stories.java
@@ -1,5 +1,12 @@
 public class stories extends Posting{
+    public stories(String url, String user_id, String contents, String photos_url, String videos_url) {
+        super();
+        //TODO Auto-generated constructor stub
+    }
     //story에 대한 답장이나 이모티콘은 계정의 owner만 볼 수 있기에 private로 선언하였음.
-    private String reply;
-    private int emoticon_bits;
+   
+    public void UserViews(){
+        // System.out.println("이 곳에 view를 볼 수 있는 규격에 맞게 출력하는 메소드설정");
+
+    }
 }
